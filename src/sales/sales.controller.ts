88dc83326969e177ddc8ca302f0211359b38dc1d
ed-breno <breno.henrique@empreenderdinheiro.com>
2024-12-09 22:59:02 -0300

    :recuycle: using  the use cases of the sales

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -1,34 +1,47 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SalesService } from './sales.service';
-import { CreateSaleDto } from './dto/create-sale.dto';
-import { UpdateSaleDto } from './dto/update-sale.dto';
+import { CreateSalesUseCase } from './usecases/createSales';
+import { GetAllSalesUseCase } from './usecases/getAllSales';
+import { GetOneSaleUseCase } from './usecases/getOneSale';
+import { UpdateSalesUseCase } from './usecases/updateSale';
+import { DeleteSalesUseCase } from './usecases/deleteSale';
+import { CreateSalesDto } from './dto/create-sale.dto';
+import { UpdateSalesDto } from './dto/update-sale.dto';
+
 
 @Controller('sales')
 export class SalesController {
-  constructor(private readonly salesService: SalesService) {}
+  constructor(
+
+    private readonly createSales: CreateSalesUseCase,
+    private readonly getAllSales: GetAllSalesUseCase  ,
+    private readonly getOneSales: GetOneSaleUseCase ,
+    private readonly updateSales: UpdateSalesUseCase,
+    private readonly deleteSales: DeleteSalesUseCase
+  ) {}
 
   @Post()
-  create(@Body() createSaleDto: CreateSaleDto) {
-    return this.salesService.create(createSaleDto);
+  create(@Body() createSaleDto: CreateSalesDto) {
+    return this.createSales.execute(createSaleDto);
   }
 
   @Get()
   findAll() {
-    return this.salesService.findAll();
+    return this.getAllSales.execute();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.salesService.findOne(+id);
+    return this.getOneSales.execute(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSaleDto: UpdateSaleDto) {
-    return this.salesService.update(+id, updateSaleDto);
+  update(@Param('id') id: string, @Body() updateSaleDto: UpdateSalesDto) {
+    return this.updateSales.execute(id, updateSaleDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.salesService.remove(+id);
+    return this.deleteSales.execute(id);
   }
 }
